2622a5e02a45372bf32c96dd13ca5064e26a0154
bshmat <bogdan.shmat@wsu.edu>
2023-05-24 17:27:25 -0700

    added a ratelimiter, 3 entries per 1 minute

--- src/server/api/routers/food.ts
+++ src/server/api/routers/food.ts
@@ -1,8 +1,24 @@
 //src/server/api/routers/food.ts
 // Import necessary dependencies
 import { TRPCError } from "@trpc/server";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure, privateProcedure } from "~/server/api/trpc";
+
+
+// Create a new ratelimiter, that allows 1 requests per 1 minute
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
+  /**
+   * Optional prefix for the keys used in redis. This is useful if you want to share a redis
+   * instance with other applications and want to avoid key collisions. The default prefix is
+   * "@upstash/ratelimit"
+   */ 
+  //prefix: "@upstash/ratelimit",
+});
 
 // Create and export a router for food-related routes
 export const foodRouter = createTRPCRouter({
@@ -44,6 +60,10 @@ export const foodRouter = createTRPCRouter({
     )
     .mutation(async ({ ctx, input }) => {
       const userId = ctx.userId;
+
+      const { success } = await ratelimit.limit(userId);
+
+      if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
 
       // Check if the user ID exists. If not, throw an error.
       if (!userId) {
