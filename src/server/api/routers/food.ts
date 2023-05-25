e023438dbeced52062bebe5993f9c2da50ae31c9
bshmat <bogdan.shmat@wsu.edu>
2023-05-24 18:00:29 -0700

    upped ratelimit to 50/min

--- src/server/api/routers/food.ts
+++ src/server/api/routers/food.ts
@@ -10,7 +10,7 @@ import { createTRPCRouter, publicProcedure, privateProcedure } from "~/server/ap
 // Create a new ratelimiter, that allows 1 requests per 1 minute
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  limiter: Ratelimit.slidingWindow(50, "1 m"),
   analytics: true,
   /**
    * Optional prefix for the keys used in redis. This is useful if you want to share a redis
