f4eebeeb141d74c87fe0c0fdac3f0520a96b7beb
bshmat <bogdan.shmat@wsu.edu>
2023-05-28 00:46:08 -0700

    redeploy

--- src/components/util/Buttons.tsx
+++ src/components/util/Buttons.tsx
@@ -2,19 +2,6 @@
 
 import React from 'react';
 
-type AddFoodButtonProps = {
-  onClick?: () => void;
-  type?: "button" | "submit" | "reset";
-};
-
-export const AddFoodButton: React.FC<AddFoodButtonProps> = ({ onClick }) => (
-  <button onClick={onClick} className="btn btn-circle">
-    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v12M6 12h12" />
-    </svg>
-  </button>
-);
-
 type GoogleLoginButtonProps = {
   onClick?: () => void;
 };
