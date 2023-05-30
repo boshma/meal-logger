0767c579fe04e772e82bd2f8d67796d3b621b2ac
bshmat <bogdan.shmat@wsu.edu>
2023-05-29 20:23:28 -0700

    landing page makeover

--- src/components/loginpage.tsx
+++ src/components/loginpage.tsx
@@ -1,21 +1,23 @@
 // src/components/LoginPage.tsx
-
 import React from 'react';
 import { GoogleLoginButton } from './util/Buttons';
 import { SignInButton } from "@clerk/nextjs";
 
 const LoginPage: React.FC = () => (
-  <div className="hero min-h-screen bg-base-200">
-    <div className="hero-content text-center">
-      <div className="max-w-md">
-        <h1 className="text-5xl font-bold">Welcome to Fitache </h1>
-        <p className="py-6">The Ultimate tool for tracking your macros built for user ease.</p>
-        <SignInButton>
-          <GoogleLoginButton />
-        </SignInButton>
+  <div className="min-h-screen bg-background flex items-center justify-center">
+    <div className="container max-w-md p-6 rounded-lg bg-card shadow-xl transform transition-transform duration-500 ease-in-out hover:scale-105">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold text-primary">Welcome to Fitache </h1>
+        <p className="py-6 text-foreground">The most convienent macrotracking app.</p>
+        <div className="animate-accordion-down">
+          <SignInButton>
+            <GoogleLoginButton />
+          </SignInButton>
+        </div>
       </div>
     </div>
   </div>
 );
 
 export default LoginPage;
+
