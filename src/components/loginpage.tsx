e0a9d8657f7f1c9616decec1ddf0e32258c805e6
Bogdan Shmat <Bogdan.shmat@invent.us>
2023-07-18 15:33:40 -0700

    fix: LCP fix , removed FM

--- src/components/loginpage.tsx
+++ src/components/loginpage.tsx
@@ -3,13 +3,12 @@ import React, { useState } from 'react';
 import { SignInButton } from "@clerk/nextjs";
 import { Button } from './ui/button';
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from './ui/card';
-import { AnimatePresence, motion } from "framer-motion";
 
 const backgroundColors = [
-  'radial-gradient(circle at center, #050A30, #1F3467, #3D609B)',
-  'radial-gradient(circle at center, #050A30, #2A165E, #763D8C)', 
-  'radial-gradient(circle at center, #050A30, #3D609B, #C1D6F2)', 
-  'radial-gradient(circle at center, #1F3467, #763D8C, #C1D6F2)', 
+  'bg-gradient-1',
+  'bg-gradient-2',
+  'bg-gradient-3',
+  'bg-gradient-4',
 ];
 let colorIndex = 0;
 
@@ -29,26 +28,7 @@ const LoginPage: React.FC = () => {
   }
 
   return (
-    <div className="relative min-h-screen flex items-center justify-center px-4 sm:px-6">
-      <AnimatePresence mode='wait'>
-        <motion.div
-          key={background}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.5 }}
-          style={{
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            background,
-            zIndex: -1
-          }}
-        />
-
-      </AnimatePresence>
+    <div className={`relative min-h-screen flex items-center justify-center px-4 sm:px-6 ${background} bg-transition`} style={{zIndex: -1}}>
       <Card className="container max-w-md py-8 px-6 transform transition-transform duration-500 ease-in-out hover:scale-105 hover:shadow-lg">
         <CardHeader>
           <CardTitle className="text-center text-4xl sm:text-5xl font-extrabold text-primary tracking-tighter font-heading animate-slideInFromTop">
