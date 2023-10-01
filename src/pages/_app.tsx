b85935d71d082d08274d935ef31f71166dc2ccbb
Bogdan Shmat <Bogdan.shmat@invent.us>
2023-10-01 13:18:20 -0700

    add: color theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,9 +6,13 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "react-hot-toast";
+import {shadesOfPurple} from "@clerk/themes";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <ClerkProvider {...pageProps}>
+  return <ClerkProvider  appearance={{
+    baseTheme: shadesOfPurple,  
+  }} {...pageProps}>
+    
     <Toaster />
     <Component {...pageProps} />
   </ClerkProvider>
